fc61bfb2c1f4aa132ab979eaaa18e5a3e1147512
javidanz <javidan.zeynalov@socar.com>
2025-09-30 17:50:49 +0400

    First commit with venv

--- main.py
+++ main.py
@@ -6,17 +6,15 @@ import datetime
 import time
 import signal
 
-
-
 # Code will stop executing after 720 seconds == 10 mins.
 signal.alarm(720) 
 
-
-
 now = datetime.datetime.now()
 current_time = now.strftime("%H:%M / %A")
+
  # %A is to get the name of the Day!
 justtime = now.strftime("%H:%M")
+
 print (current_time)
 
 #  Code to allow access for Microphone, Camera and notifications
@@ -25,6 +23,7 @@ opt = Options()
 opt.add_argument("--disable-infobars")
 opt.add_argument("start-maximized")
 opt.add_argument("--disable-extensions")
+
 # Pass the argument 1 to allow and 2 to block
 opt.add_experimental_option("prefs", { \
 "profile.default_content_setting_values.media_stream_mic": 1, 
@@ -33,6 +32,7 @@ opt.add_experimental_option("prefs", { \
 "profile.default_content_setting_values.notifications": 1 
 })
 
+
 # Conditions to check time and append if necessary
 while justtime !=  "09:50" or justtime != "13:50" or justtime != "15:20" or  justtime != "16:50":
     time.sleep(20)
@@ -40,6 +40,7 @@ while justtime !=  "09:50" or justtime != "13:50" or justtime != "15:20" or  jus
     current_time = now.strftime("%H:%M / %A")
     justtime = now.strftime("%H:%M")
     print(justtime)
+
     if justtime == "09:50" or justtime == "13:50" or justtime == "15:20" or  justtime == "16:50":
         print("Class is going to start in 10 Minutes.")
         break
@@ -74,7 +75,7 @@ def gmail_login():
 
 # Conditions which checks the time and goes to the classlink if Classes are happening at that time.
 
-# Math
+# For each class
 if current_time == "09:50 / Monday" or current_time == "16:50 / Tuesday" or current_time == "13:50 / Thursday" or current_time == "15:20 / Friday":
     #sub is the class id with the meet link. sub changes with the time accoriding to the class.
     sub = "###hangouts meet links here with time variations###"
@@ -89,44 +90,11 @@ elif current_time == "13:50 / Monday" or current_time == "16:50 / Wednesday":
     driver = webdriver.Chrome(chrome_options=opt, executable_path=r'chromedriver')
     gmail_login()
     
-
-# Nepali 
-elif current_time == "15:20 / Monday" or current_time == "16:50 / Thursday" :
-    sub = "###hangouts meet links here with time variations###"
-    driver = webdriver.Chrome(chrome_options=opt, executable_path=r'chromedriver')
-    gmail_login()
-    
-
-#Chemistry 1
-elif current_time == "16:50 / Monday" or current_time == "15:20 / Wednesday":
-    sub = "###hangouts meet links here with time variations###"
-    driver = webdriver.Chrome(chrome_options=opt, executable_path=r'chromedriver')
-    gmail_login()
-    
-
-#Chemistry 2
-elif current_time == "13:52 / Tuesday" or current_time == "16:50 / Friday":
-    sub = "###hangouts meet links here with time variations###"
-    driver = webdriver.Chrome(chrome_options=opt, executable_path=r'chromedriver')
-    gmail_login()
-    
-
-# English 
-elif current_time == "15:20 / Tuesday" or current_time == "13:50 / Friday":
-    sub = "###hangouts meet links here with time variations###"
-    driver = webdriver.Chrome(chrome_options=opt, executable_path=r'chromedriver')
-    gmail_login()
-    
-
 # Physics 2
 elif current_time == "13:50 / Wednesday" or current_time == "15:20 / Thursday":
     sub = "###hangouts meet links here with time variations###"
     driver = webdriver.Chrome(chrome_options=opt, executable_path=r'chromedriver')
     gmail_login()
     
-
 else:
     print("No classes right now")
-
-
-    
